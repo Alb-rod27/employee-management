4968c252f499703cc5e34a857b96091e10c136d7
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-06-05 20:56:51 -0700

    fixed previous repo issue with server

---
+++ schema.sql
@@ -0,0 +1,30 @@
+
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+USE employee_DB;
+
+CREATE TABLE department (
+    dep_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (dep_id)
+);
+
+CREATE TABLE role (
+    r_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (r_id),
+    FOREIGN KEY (department_id) REFERENCES department(dep_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    emp_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (emp_id),
+    FOREIGN KEY (role_id) REFERENCES role(r_id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id) ON DELETE CASCADE
+);
---
+++ seeds.sql
@@ -0,0 +1,64 @@
+INSERT INTO department (dep_id, name)
+VALUES (1, "Sales");
+
+INSERT INTO department (dep_id, name)
+VALUES (2, "Legal");
+
+INSERT INTO department (dep_id, name)
+VALUES (3, "Engineering");
+
+INSERT INTO department (dep_id, name)
+VALUES (4, "Finance");
+
+-- role seeds
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 1);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (2, "Salesperson", 80000, 1);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (3, "Legal Team Lead", 250000, 2);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (4, "Lawyer", 190000, 2);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (5, "Lead Engineer", 150000, 3);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (6, "Software Egineer", 120000, 3);
+
+INSERT INTO role (r_id, title, salary, department_id)
+VALUES (7, "Accountant", 125000, 4);
+
+-- employee seeds
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe", 1, 3);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Mike", "Chan", 2, 1);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id)
+VALUES (3, "Ashley", "Rodriguez", 5);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Robert", "Lopez", 6, 3);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Rick", "Sanchez", 6, 3);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id)
+VALUES (6, "Malia", "Brown", 7);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id)
+VALUES (7, "Sarah", "Lourd", 3);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Tom", "Allen", 4, 7);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Zack", "Martin", 7, 8);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Tammy", "Galal", 6, 3);
